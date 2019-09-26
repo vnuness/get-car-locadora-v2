6c016d25ce84055228087d27cf3d973e83e6c32c
Victor Santana Nunes da Silva <vsilva@meeta.com.br>
2019-09-26 19:57:53 -0300

    #

--- config/permissions.php
+++ config/permissions.php
@@ -17,30 +17,30 @@ return [
                         'veiculos.view' => 'Visualizar página',
                     ]
                 ],
-                "Credenciais" => [
-                    "menu" => ['type' => 'sub-menu', "icon-class" => "fa fa-address-book-o"],
-                    "itens" => [
-                        "Perfis" => [
-                            "menu" => ["type" => "link", "route" => "credentials.profiles.index"],
-                            "permissions" => [
-                                'credentials.profiles.list' => 'Listar',
-                                'credentials.profiles.create' => 'Cadastrar',
-                                'credentials.profiles.show' => 'Detalhes',
-                                'credentials.profiles.edit' => 'Editar',
-                                'credentials.profiles.delete' => 'Apagar'
-                            ]
-                        ],
-                        "Usuários" => [
-                            "menu" => ["type" => "link", "route" => "credentials.users.index"],
-                            "permissions" => [
-                                'credentials.users.list' => 'Listar',
-                                'credentials.users.create' => 'Cadastrar',
-                                'credentials.users.edit' => 'Editar',
-                                'credentials.users.delete' => 'Apagar'
-                            ]
-                        ]
-                    ]
-                ]
+//                "Credenciais" => [
+//                    "menu" => ['type' => 'sub-menu', "icon-class" => "fa fa-address-book-o"],
+//                    "itens" => [
+//                        "Perfis" => [
+//                            "menu" => ["type" => "link", "route" => "credentials.profiles.index"],
+//                            "permissions" => [
+//                                'credentials.profiles.list' => 'Listar',
+//                                'credentials.profiles.create' => 'Cadastrar',
+//                                'credentials.profiles.show' => 'Detalhes',
+//                                'credentials.profiles.edit' => 'Editar',
+//                                'credentials.profiles.delete' => 'Apagar'
+//                            ]
+//                        ],
+//                        "Usuários" => [
+//                            "menu" => ["type" => "link", "route" => "credentials.users.index"],
+//                            "permissions" => [
+//                                'credentials.users.list' => 'Listar',
+//                                'credentials.users.create' => 'Cadastrar',
+//                                'credentials.users.edit' => 'Editar',
+//                                'credentials.users.delete' => 'Apagar'
+//                            ]
+//                        ]
+//                    ]
+//                ]
             ]
         ]
     ],
